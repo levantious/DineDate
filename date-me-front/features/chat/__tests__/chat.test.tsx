b1bda6c1508b950a2427e61b1bceea798a2d7468
Odin Thif <mjd.thif@icloud.com>
2025-01-25 04:20:17 +0100

    set up mocking tests for sending and recieving messages

--- date-me-front/features/chat/__tests__/chat.test.tsx
+++ date-me-front/features/chat/__tests__/chat.test.tsx
@@ -1,13 +1,67 @@
 import React from "react";
-import { render } from "@testing-library/react-native";
-import { UserInfo } from "../ui";
+import { render, fireEvent, waitFor, act } from "@testing-library/react-native";
+import { startConnection, sendMessage, onReceiveMessage } from "../services";
+import ChatScreen from "@/app/(tabs)/chat";
 
-test("renders the name prop correctly", () => {
-  const testName = "John Doe";
+jest.mock("../services", () => ({
+  startConnection: jest.fn(),
+  sendMessage: jest.fn(),
+  onReceiveMessage: jest.fn(),
+}));
 
-  const { getByText } = render(
-    <UserInfo name={testName} isOnline={true} timeSent="10:00 AM" />
-  );
+describe("ChatScreen Component", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-  expect(getByText(testName)).toBeTruthy();
+  test("does not send message if message input is empty | 0 case", async () => {
+    const { getByPlaceholderText, getByText } = render(<ChatScreen />);
+    const messageInput = getByPlaceholderText("Enter your message");
+    const sendButton = getByText("Send");
+
+    await act(async () => {
+      fireEvent.changeText(messageInput, "Hello, world!");
+      fireEvent.press(sendButton);
+    });
+
+    expect(sendMessage).not.toHaveBeenCalled();
+  });
+
+  test("handles user input and sends message | 1 case", async () => {
+    (startConnection as jest.Mock).mockResolvedValue({});
+    const { getByPlaceholderText, getByText } = render(<ChatScreen />);
+
+    const usernameInput = getByPlaceholderText("Enter your username");
+    const messageInput = getByPlaceholderText("Enter your message");
+    const sendButton = getByText("Send");
+
+    fireEvent.changeText(usernameInput, "testuser");
+    fireEvent.changeText(messageInput, "Hello, world!");
+    fireEvent.press(sendButton);
+
+    expect(sendMessage).toHaveBeenCalledWith("testuser", "Hello, world!");
+  });
+  test("renders input fields and send button", () => {
+    const { getByPlaceholderText, getByText } = render(<ChatScreen />);
+
+    const messageInput = getByPlaceholderText("Enter your message");
+    const sendButton = getByText("Send");
+
+    expect(messageInput).toBeTruthy();
+    expect(sendButton).toBeTruthy();
+  });
+
+  test("receives and displays messages", async () => {
+    const mockMessage = { user: "testuser", message: "Hello, world!" };
+    (startConnection as jest.Mock).mockResolvedValue({});
+    (onReceiveMessage as jest.Mock).mockImplementation((callback) => {
+      callback(mockMessage.user, mockMessage.message);
+    });
+
+    const { getByText } = render(<ChatScreen />);
+
+    await waitFor(() => {
+      expect(getByText("testuser: Hello, world!")).toBeTruthy();
+    });
+  });
 });
