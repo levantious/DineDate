11d129011e8ccb3321ccaddb64f0885057da7739
Odin Thif <mjd.thif@icloud.com>
2025-03-15 00:09:28 +0100

    adding auth session for Tablayout

--- src/gui/mobile/app/(tabs)/_layout.tsx
+++ src/gui/mobile/app/(tabs)/_layout.tsx
@@ -1,10 +1,27 @@
-import { Tabs } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
 import React from "react";
 import { Platform } from "react-native";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { HapticTab, IconSymbol } from "@/components/ui";
-
+import { useSession } from "@/features/auth/services";
+import { Text } from 'react-native';
 export default function TabLayout() {
+  const { session, isLoading } = useSession();
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
+
+  // Only require authentication within the (app) group's layout as users
+  // need to be able to access the (auth) group and sign in again.
+  if (!session) {
+    // On web, static rendering will stop here as the user is not authenticated
+    // in the headless Node process that the pages are rendered in.
+    return <Redirect href="/login" />;
+  }
+
+  // This layout can be deferred because it's not the root layout.
+
   return (
     <Tabs
       screenOptions={{
