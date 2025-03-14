f0b5235ba9c2c30a9be5b7b30bad62e142a8ce44
Odin Thif <mjd.thif@icloud.com>
2025-03-14 22:58:30 +0100

    fix importing react lib

--- src/gui/mobile/_layout.tsx
+++ src/gui/mobile/_layout.tsx
@@ -1,8 +1,10 @@
+import React from "react";
+import { useEffect } from "react";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
+
 import "react-native-reanimated";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -26,6 +28,9 @@ export default function RootLayout() {
   return (
     <>
       <Stack>
+        <Stack.Screen name="Login" options={{ headerShown: false }} />
+        <Stack.Screen name="Register" />
+        <Stack.Screen name="Home" />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
