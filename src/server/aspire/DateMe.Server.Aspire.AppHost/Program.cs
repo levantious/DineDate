adedc1aba86a4124ff173586997bf510ddae94cf
TMS-Namespace <code.forkedly@slmail.me>
2025-03-20 23:20:10 +0100

    gui docker file and aspire

--- src/server/aspire/DateMe.Server.Aspire.AppHost/Program.cs
+++ src/server/aspire/DateMe.Server.Aspire.AppHost/Program.cs
@@ -2,6 +2,11 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var server = builder.AddProject<Projects.DateMe_Server_Chat>("SignalRServer");
+var server = builder.AddProject<Projects.DateMe_Server_Chat>("server");
+
+var client = builder.AddDockerfile("client", "../../../gui", "Dockerfile")
+                    .WithHttpEndpoint(port: 8082, targetPort:8081)
+                    .WithReference(server)
+                    .WaitFor(server);
 
 builder.Build().Run();
