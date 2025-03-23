238bc0b53297ae7c824b6d9da1ab02a019c57b8a
TMS-Namespace <code.forkedly@slmail.me>
2025-03-23 23:27:29 +0100

    WIP

--- src/server/aspire/DateMe.Server.Aspire.AppHost/Program.cs
+++ src/server/aspire/DateMe.Server.Aspire.AppHost/Program.cs
@@ -1,12 +1,24 @@
+using dotenv.net;
 using Projects;
+
+var envVars = DotEnv
+    .Fluent()
+    .WithExceptions()
+    .WithEnvFiles("../../.env")
+    .WithTrimValues()
+    .Read();
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var server = builder.AddProject<Projects.DateMe_Server_Chat>("server");
+var server = builder
+    .AddProject<Projects.DateMe_Server_Chat>("server")
+    .WithEnvironment("ASPNETCORE_URLS", envVars["ASPNETCORE_URLS"]);
 
-var client = builder.AddDockerfile("client", "../../../gui", "Dockerfile")
-                    .WithHttpEndpoint(port: 8082, targetPort:8081)
-                    .WithReference(server)
-                    .WaitFor(server);
+var client = builder
+        .AddDockerfile("client", "../../../gui", "Dockerfile")
+        .WithHttpEndpoint(port: 8082, targetPort:8081)
+        .WithEnvironment()
+        .WithReference(server)
+        .WaitFor(server);
 
 builder.Build().Run();
